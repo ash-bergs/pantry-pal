72fc2997cc542bb57fbf3f7d89f3a1f060ba870f
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-07-31 15:59:28 -0400

    overhaul the total price and controls UI, add toggle for list filters

--- index.js
+++ index.js
@@ -11,6 +11,17 @@ const itemForm = document.getElementById('itemForm');
 const itemsDiv = document.getElementById('itemsDiv');
 const priceAmount = document.getElementById('priceAmount');
 const clearFormButton = document.getElementById('cancelButton');
+// form inputs and err msgs
+const nameInput = document.getElementById('nameInput');
+const nameError = document.getElementById('nameError');
+const priceInput = document.getElementById('priceInput');
+const priceError = document.getElementById('priceError');
+// show/hide filter ui
+let isItemsControlHidden = false;
+const showItemsControlContainer = document.getElementById('showItemsControl');
+const itemsControl = document.getElementById('itemsControl');
+const toggleMessage = document.getElementById('toggleMessage');
+const toggleArrow = document.getElementById('arrow');
 
 // fetch items from the database
 const populateItemsDiv = async () => {
@@ -27,7 +38,7 @@ const populateItemsDiv = async () => {
   itemsDiv.innerHTML = sortedItems
     .map(
       (item) => `
-      <div class="item ${item.isPurchased && 'purchased'}">
+      <div class="item ${item.isPurchased && 'purchased'}" id="item-${item.id}">
         
           <input
            type="checkbox" 
@@ -48,7 +59,7 @@ const populateItemsDiv = async () => {
             <p class="itemQuantityText">${item.quantity}</p>
           </div>
           <div class="itemPriceContainer">
-            <p class="itemInfoHeading">Price</p>
+            <p class="itemInfoHeading">Est. Price</p>
             <p class="itemPriceText">$${item.price} </p>
           </div>
         </div>
@@ -76,16 +87,49 @@ window.onload = populateItemsDiv;
 
 // add function to clear form button
 const clearForm = () => {
+  clearNameErrors();
+  clearPriceErrors();
   itemForm.reset();
   document.getElementById('nameInput').focus(); // return focus to the top form input
+};
+
+// this should take an input id?
+const clearNameErrors = () => {
+  nameInput.setCustomValidity('');
+  nameError.textContent = '';
+};
+
+const clearPriceErrors = () => {
+  priceInput.setCustomValidity('');
+  priceError.textContent = '';
 };
 
 // bind clear form button to action
 clearFormButton.addEventListener('click', clearForm);
 
+// clear input errors on change in the input
+nameInput.addEventListener('input', clearNameErrors);
+priceInput.addEventListener('input', clearPriceErrors);
+
 // Form submit
 itemForm.onsubmit = async (event) => {
   event.preventDefault();
+  // handle assigning errors
+  let valid = true;
+
+  if (!nameInput.validity.valid) {
+    nameInput.setCustomValidity('Please enter an item name');
+    nameError.textContent = nameInput.validationMessage;
+    valid = false;
+  }
+
+  if (!priceInput.validity.valid) {
+    priceInput.setCustomValidity('Please enter a valid price (e.g. $1.99)');
+    priceError.textContent = priceInput.validationMessage;
+    valid = false;
+  }
+
+  if (!valid) return;
 
   const name = document.getElementById('nameInput').value;
   const quantity = document.getElementById('quantityInput').value;
@@ -99,6 +143,28 @@ itemForm.onsubmit = async (event) => {
   document.getElementById('nameInput').focus(); // return focus to the top form input
 };
 
+// might want to rename itemsCOntrol to filter control?
+const toggleItemsControls = () => {
+  // toggle collapsed state
+  isItemsControlHidden = !isItemsControlHidden;
+
+  if (isItemsControlHidden) {
+    // Hide the controls
+    itemsControl.style.display = 'none';
+    toggleMessage.textContent = 'Show Filters';
+    toggleArrow.classList.remove('arrow-down');
+    toggleArrow.classList.add('arrow-up');
+  } else {
+    // Show the controls
+    itemsControl.style.display = 'block';
+    toggleMessage.textContent = 'Hide Filters';
+    toggleArrow.classList.remove('arrow-up');
+    toggleArrow.classList.add('arrow-down');
+  }
+};
+
+showItemsControlContainer.addEventListener('click', toggleItemsControls);
+
 const toggleItemPurchaseStatus = async (event, id) => {
   await db.items.update(id, { isPurchased: !!event.target.checked });
   await populateItemsDiv();
@@ -111,3 +177,15 @@ const removeItem = async (id) => {
 
 //todo: pantry page - when an item is removed from shopping list, add to pantry
 //todo: mass delete (all checked items)
+
+/* ---------------------------- NOTES ON SORTING ---------------------------- */
+/**
+ * Auto Sort - automatically moves checked items below unchecked items in the group
+ *
+ * Hide checked - won't show items that are checked in the list
+ *
+ * Sort by Section - Future - will sort by the category an item is in if available
+ * uncategorized items go to the bottom of the list
+ *
+ *
+ */
