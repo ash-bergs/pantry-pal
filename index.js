b18c4782fb88f1b90c374a3b48b9fe7284a256d6
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-08-01 15:45:15 -0400

    accessibility improvements 🎉

--- index.js
+++ index.js
@@ -3,6 +3,83 @@ import { toggleItemsControls } from './modules/itemsControl.js';
 import { populateItems } from './modules/populateItems.js';
 import db from './modules/db.js';
 
+// get modal and focusable elements
+const modal = document.getElementById('modal');
+const openModalButton = document.getElementById('openModalButton');
+const closeModalButton = document.getElementById('closeModal');
+// define the types of focusable elements
+const focusableElements =
+  'button, [href], input, select, textarea, [tabindex]:not([tabindex="-1"])';
+const firstFocusableElement = modal.querySelectorAll(focusableElements)[0];
+const focusableContent = modal.querySelector(focusableElements);
+const lastFocusableElement = focusableContent[focusableContent.length - 1];
+
+// utility to set the modal inert
+// make all elements BEHIND the modal non-interactive
+const setInert = (state) => {
+  document.querySelectorAll('body > *:not(.modal)').forEach((element) => {
+    element.classList.toggle('inert', state);
+  });
+};
+
+const showModal = () => {
+  modal.style.display = 'block';
+  modal.setAttribute('aria-hidden', 'false');
+  setInert(true);
+  firstFocusableElement.focus();
+};
+
+const hideModal = () => {
+  modal.style.display = 'none';
+  modal.setAttribute('aria-hidden', 'true');
+  setInert(false);
+  openModalButton.focus();
+};
+
+// When the user clicks the button, open the modal
+openModalButton.addEventListener('click', showModal);
+
+// When the user clicks on <span> (x), close the modal
+closeModalButton.addEventListener('click', hideModal);
+
+// When the user clicks anywhere outside of the modal, close it
+window.addEventListener('click', (event) => {
+  if (event.target === modal) {
+    hideModal();
+  }
+});
+
+// When the user presses the Escape key, close the modal
+window.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    hideModal();
+  }
+});
+
+// Trap focus inside the modal
+modal.addEventListener('keydown', (event) => {
+  let isTabPressed = event.key === 'Tab' || event.keyCode === 9;
+
+  if (!isTabPressed) {
+    return;
+  }
+
+  if (event.shiftKey) {
+    // if shift key pressed for shift + tab combination
+    if (document.activeElement === firstFocusableElement) {
+      lastFocusableElement.focus(); // add focus for the last focusable element
+      event.preventDefault();
+    }
+  } else {
+    // if tab key is pressed
+    if (document.activeElement === lastFocusableElement) {
+      // if focused has reached to last focusable element then focus first focusable element after pressing tab
+      firstFocusableElement.focus(); // add focus for the first focusable element
+      event.preventDefault();
+    }
+  }
+});
+
 // call populate to load shopping list items
 window.onload = populateItems;
 
