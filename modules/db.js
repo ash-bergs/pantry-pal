9b21b4c05c10993b55985b495aaeb4a8055b2b31
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-08-01 11:38:56 -0400

    comments for next part of work

--- modules/db.js
+++ modules/db.js
@@ -1,9 +1,21 @@
-const db = new Dexie('Shopping App');
+const db = new Dexie('pantry-pal');
 
 // define the structure of the db
 // if we change this after it's been created we need to increment the version and define it
 db.version(1).stores({
   items: '++id, name, price, isPurchased',
 });
+
+// next version
+// add: store section - should match an enum
+// enum values - produce, dairy, ethnic foods, meat dept, deli, baking, spice, snack, frozen, drinks
+// quantity unit - also an enum
+// enum - pounds, ounces, units, box
+// createdAt - date
+// updatedAt - date
+// db.version(2).stores({
+//   items:
+//     '++id, name, price, isPurchased, section, quantityUnit, createdAt, updatedAt',
+// });
 
 export default db;
