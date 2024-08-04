7549bc1ff68838f395f65625a5151497660d5935
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-08-04 14:38:47 -0400

    adds createdat and updatedat fields, automatically handled with dbcore from dexie

--- modules/db.js
+++ modules/db.js
@@ -13,9 +13,67 @@ db.version(1).stores({
 // enum - pounds, ounces, units, box
 // createdAt - date
 // updatedAt - date
-// db.version(2).stores({
-//   items:
-//     '++id, name, price, isPurchased, section, quantityUnit, createdAt, updatedAt',
+db.version(2).stores({
+  items:
+    '++id, name, price, isPurchased, section, quantityUnit, createdAt, updatedAt',
+});
+
+// These work - https://dexie.org/docs/Table/Table.hook('creating')
+// but we have another option available to us (see below)
+// dexie hooks to handle creation and update
+// db.items.hook('creating', (primKey, obj, trans) => {
+//   obj.createdAt = new Date().toISOString();
+//   obj.updatedAt = obj.createdAt;
 // });
+
+// db.items.hook('updating', (modifications, primKey, obj, trans) => {
+//   modifications.updatedAt = new Date().toISOString();
+//   return modifications;
+// });
+
+// set created and updated at times automatically based on triggers:
+
+// we have another option outside of the hooks available to us: https://dexie.org/docs/DBCore/DBCore
+// define dbcore middleware
+const timestampsMiddleware = {
+  stack: 'dbcore',
+  name: 'timestampsMiddleware',
+  create: (downlevelDBCore) => {
+    return {
+      ...downlevelDBCore,
+      table: (tableName) => {
+        // retrieve the db table based on name
+        const downlevelTable = downlevelDBCore.table(tableName);
+
+        return {
+          ...downlevelTable,
+          // when mutating, determine if create/update
+          mutate: async (req) => {
+            if (tableName === 'items') {
+              const now = new Date().toISOString();
+
+              if (req.type === 'add') {
+                req.values.forEach((item) => {
+                  item.createdAt = now;
+                  item.updatedAt = now;
+                });
+              } else if (req.type === 'put') {
+                // only update the updatedAt on put req
+                req.values.forEach((item) => {
+                  item.updatedAt = now;
+                });
+              }
+            }
+
+            return downlevelTable.mutate(req);
+          },
+        };
+      },
+    };
+  },
+};
+
+// use our timestamp middlware
+db.use(timestampsMiddleware);
 
 export default db;
