289ac8dc7a31765436edff42a1aa82a80b517778
Ash <65979049+ash-bergs@users.noreply.github.com>
2024-09-11 12:57:34 -0400

    adds 'confirm delete' modal

--- modules/list.ts
+++ modules/list.ts
@@ -16,6 +16,7 @@ import './uploadBackupForm';
 import './modal.js';
 import './optionsData.js';
 import './optionsModal';
+import './modals/confirmDelete';
 
 // import itemManager from './ItemManager';
 import itemManager from './ItemManager';
@@ -39,8 +40,6 @@ window.toggleItemPurchaseStatus =
 window.toggleAllItemsPurchaseStatus =
   itemManager.toggleAllListItems.bind(itemManager);
 window.removeItem = itemManager.removeItem.bind(itemManager);
-window.removePurchasedItems =
-  itemManager.removePurchasedItems.bind(itemManager);
 window.populateItems = itemManager.populateItems.bind(itemManager);
 
 // get download button and add action
